12bdcadfc47e49773d8a7a3e9fcb7e7d5ad9d42e
chenli <909549030@qq.com>
2018-12-01 22:16:08 +0800

    7end

--- 7ReverseInteger.cpp
+++ 7ReverseInteger.cpp
@@ -7,7 +7,6 @@ public:
     int reverse(int x) {
         int flag =(x>=0)?1:0;
         if(flag==0) x=-1*x;
-        
         int firstZero=0;
         vector<int>a;
         while(x){
@@ -22,8 +21,11 @@ public:
             result+=pow(10,firstZero)*a[i];
             firstZero++;
         }
-        if(result>pow(2,31)-1&&flag==1)return 0;
-        if(result>pow(2,31)&&flag==0)return 0;
+        //这里没有想官方那样，直接考虑溢出返回相应的补码，
+        //之前转换的时候都已经转换为正的了，所以就补码只可能为负的
+        if(result<0)return 0;
+        // if(result>pow(2,31)-1&&flag==1)return 0;
+        // if(result>pow(2,31)&&flag==0)return 0;
         return flag>0?result:-1*result;
     }
 };
